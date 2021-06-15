76a06d47bd4993413c65376264831888a4175839
linamandla <linamabdlajojwana96@gmail.com>
2021-06-15 10:11:59 +0200

    update

--- registration.js
+++ registration.js
@@ -1,26 +1,48 @@
 var addbtnElem = document.querySelector(".addbtn");
-var displayElem = document.querySelector(".diplay");
+var radiocheckedElem = document.querySelector(".regPlace")
+var displayElem = document.querySelector(".display");
 var textRegElem = document.querySelector(".regi");
+var showbtnElem  = document.querySelector(".showbtn");
+var showallbtnElem = document.querySelector(".showAllbtn");
+var resetbtnElem = document.querySelector(".resetbtn");
 
+var regex = /^[A-Z]{2}([0-9]{3})|[A-Z]{2}([0-9]{3} [0-9]{3})|[A-Z]{2}([0-9]{6})$/
 
-//let reg= registration();
-
-function addbtnClicked(){
-var regEntered = textRegElem.value;
-
-if(regEntered = ""){
-displayElem.innerHTML = "";
-}
-else{
-    displayElem.innerHTML = textRegElem.value;
-}
-//displayElem.innerHTML = reg.regNum(regEntered);
-
-
-
-
+let regiF = registration();
+let existReg;
+if (localStorage["regNum"]) {
+    existReg = JSON.parse(localStorage.getItem("regList"));
 }
 
+function addbtnClicked() {
+    
+    if(regex.test(textRegElem.value)){
+    regiF.storeRegNum(textRegElem.value)
+
+        localStorage.setItem("regNum", JSON.stringify(regiF.registeredNum()))
+    }
+        
+    
+console.log(regiF.registeredNum());
+    var spanE = document.createElement('span');
+    for(let i =0; i< regiF.registeredNum().length; i++){
+        spanE.innerHTML = regiF.registeredNum()[i];
+        displayElem.appendChild(spanE);
+    }
+    
+   
+
+}
+function showbtnClicked(){
+    var radioChecked = document.querySelector("input[name ='reg']:checked");
+    if(radioChecked =="Capetown"){
+     return regiF.capeReg();
+     
+    }
+ 
+}
 
 
-addbtnElem.addEventListener('click',addbtnClicked);
+addbtnElem.addEventListener('click', addbtnClicked);
+showbtnElem.addEventListener('click',showbtnClicked)
+//showallbtnElem.addEventListener('click',showallbtnClicked)
--- registrationFactoryFun.js
+++ registrationFactoryFun.js
@@ -1,23 +1,43 @@
-function registration(){
-
-function regNum(regEntered){
-//var regEntered = textRegElem.value;
-if(regEntered){
-
-}
-return  regEntered;
-}
+function registration(regList) {
+    var regNumlist = regList || [];
+    var regex = /(^[A-Z]{2}[0-9]{3}$)/ | /(^[A-Z]{3}[0-9]{3}(-)[0-9]{3}$)/ | /(^[A-Z]{2}[0-9]{6}$)/;
 
 
 
+    function storeRegNum(regNum) {
+        if (regNum) {
+            if (!regNumlist.includes(regNum)) {
+                regNumlist.push(regNum);
+
+            }
+
+        }
+    }
+    
+    function registeredNum() {
+        return regNumlist;
+    }
+    
+    
+    function capeReg() {
+        var townReg = [];
+        for (var i = 0; i < regNumlist.length; i++) {
+            
+            if (regNum.startWith('CA')) {
+                townReg.push(regNumlist)
+            
+            }
+        }
+        return townReg.filter(regNumlist);
+    }
 
 
 
-
-
-    return{
-regNum,
-
+    return {
+        
+        storeRegNum,
+        registeredNum,
+        capeReg,
 
     }
 }
